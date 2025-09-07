bf0faa693f77080b58c242b442ade438ea42f573
brianrecoba <brian12fernandes@gmail.com>
2025-09-07 01:52:32 -0300

    feat: added json data

---
+++ src/data/cards.ts
@@ -0,0 +1,6 @@
+export const cards = [
+  { id: 1, title: "Enviar", icon: require("../assets/enviar.png") },
+  { id: 2, title: "Receber", icon: require("../assets/receber.png") },
+  { id: 3, title: "Cartões", icon: require("../assets/card.png") },
+  { id: 4, title: "Bloquear", icon: require("../assets/bloquear.png") },
+];
---
+++ src/data/paymentCard.ts
@@ -0,0 +1,18 @@
+export const PaymentCard = [
+  {
+    id: 1,
+    cardHolder: "Pâmela Silva",
+    cardNumber: "1234 5678 9012 3456",
+    expiry: "12/24",
+    cvv: "123",
+    flag: require("../assets/visa.png"),
+  },
+  {
+    id: 2,
+    cardHolder: "Pâmela Silva",
+    cardNumber: "9876 5432 1098 7654",
+    expiry: "11/25",
+    cvv: "456",
+    flag: require("../assets/mastercard.png"),
+  },
+];
