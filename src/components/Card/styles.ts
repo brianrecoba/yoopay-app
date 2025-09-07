70f97ec0db7da7f559e00621ed46e5c97191b56c
brianrecoba <brian12fernandes@gmail.com>
2025-09-07 01:52:48 -0300

    feat: create component card

---
+++ src/components/Card/styles.ts
@@ -0,0 +1,11 @@
+import { theme } from "@/styles/theme";
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    width: 337,
+    height: 184,
+    borderRadius: 16,
+    backgroundColor: theme.colors.primary,
+  },
+});
