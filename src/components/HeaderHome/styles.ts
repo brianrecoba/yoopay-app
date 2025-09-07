27c32af54f1a5f044eb1520d83445f454dd7ae0b
brianrecoba <brian12fernandes@gmail.com>
2025-09-07 01:53:36 -0300

    style: adjusted to padding Header

--- src/components/HeaderHome/styles.ts
+++ src/components/HeaderHome/styles.ts
@@ -7,7 +7,6 @@ export const styles = StyleSheet.create({
     flexDirection: "row",
     justifyContent: "flex-start",
     alignItems: "center",
-    paddingHorizontal: 25,
     paddingVertical: 24,
   },
   imageContainer: {},
