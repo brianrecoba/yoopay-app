233d17739006edec9fb1895ba4421433a745c20c
brianrecoba <brian12fernandes@gmail.com>
2025-09-23 17:01:25 -0300

    chore: create data of expenses

---
+++ src/data/expenses.ts
@@ -0,0 +1,23 @@
+export const expenses = [
+  {
+    icon: require("../assets/netflix-logo.png"),
+    name: "Netflix",
+    day: "28",
+    mounth: "maio",
+    price: "R$ 44,90",
+  },
+  {
+    icon: require("../assets/ifood-logo.png"),
+    name: "Ifood",
+    day: "17",
+    mounth: "maio",
+    price: "R$ 71,54",
+  },
+  {
+    icon: require("../assets/vivo-logo.png"),
+    name: "Vivo",
+    day: "14",
+    mounth: "maio",
+    price: "R$ 39,99",
+  },
+];
